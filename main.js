7bb7de90bd4a876906880f8cb52e443117e47a53
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-09-26 11:41:42 -0300

    e

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ function soma (numero1,numero2){
     return numero1+numero2;
 }
 console.log(soma(1,2));
-function multiplicação(numero1,numero2){
-    return numero1*numero2/2
+
+function areaQuadrado(lado){
+    return lado**2;
 }
+console.log(areaQuadrado(6));
