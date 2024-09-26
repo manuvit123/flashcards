d9fe58d82ec8d1e05d675b34551a9975e2360e8b
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-09-26 12:24:48 -0300

    m

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ function areaQuadrado(lado){
     return lado**2;
 }
 console.log(areaQuadrado(numero));
+
+let media=function(numero1,numero2,numero3){
+    return numero1+numero2+numero3/3;
+}
+document.write(numero)
