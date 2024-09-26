61cbe1d74b7d8c33a95fccb1478864c35d40e94f
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-09-26 12:13:06 -0300

    b

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let numero=parseInt(prompt('numero1'));
+document.write(numero)
 function soma (numero1,numero2){
     return numero1+numero2;
 }
@@ -6,4 +8,4 @@ console.log(soma(1,2));
 function areaQuadrado(lado){
     return lado**2;
 }
-console.log(areaQuadrado(6));
+console.log(areaQuadrado(numero));
