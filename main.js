9090cbe4db6cddfa1fadc8223985bd940a929c10
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-10-03 12:07:31 -0300

    n

--- main.js
+++ main.js
@@ -21,7 +21,7 @@
 
 let estados = ['AC','AL','AP','AM','BA','CE','ES','DF','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
 let i = 0;
-while(i<estado.length){
+while(i<estados.length){
     console.log (i);
     console.log(estados[i]);
     i=i+1;
