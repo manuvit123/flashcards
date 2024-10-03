0a8b3ad893c2e0f4c0e492b9a60308cd2c312fa8
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-10-03 12:27:14 -0300

    g

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-// let numero=parseInt(prompt('numero'));
-// document.write(numero)
-// function soma (numero1,numero2){
-//     return numero1+numero2;
+// // let numero=parseInt(prompt('numero'));
+// // document.write(numero)
+// // function soma (numero1,numero2){
+// //     return numero1+numero2;
+// // }
+// // console.log(soma(1,2));
+
+// // function areaQuadrado(lado){
+// //     return lado**2;
+// // }
+// // console.log(areaQuadrado(numero));
+
+// // let numero1 = parseInt(prompt('Qual numero?'))
+// // let numero2 = parseInt(prompt('Qual numero?'))
+// // let numero3 = parseInt(prompt('Qual numero?'))
+
+// // function media (x,y,z){
+// //     return (x+y+z)/3;
+// // }
+// // document.write(media(numero1,numero2,numero3))
+
+// let estados = ['AC','AL','AP','AM','BA','CE','ES','DF','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
+// let i = 0;
+// while(i<estados.length){
+//     console.log (i);
+//     console.log(estados[i]);
+//     i++;
 // }
-// console.log(soma(1,2));
 
-// function areaQuadrado(lado){
-//     return lado**2;
+// for (let index = 0; index < estados.length; index++) {
+//     console.log(index);
+//     console.log(estados[index]);
 // }
-// console.log(areaQuadrado(numero));
 
-// let numero1 = parseInt(prompt('Qual numero?'))
-// let numero2 = parseInt(prompt('Qual numero?'))
-// let numero3 = parseInt(prompt('Qual numero?'))
-
-// function media (x,y,z){
-//     return (x+y+z)/3;
-// }
-// document.write(media(numero1,numero2,numero3))
-
-let estados = ['AC','AL','AP','AM','BA','CE','ES','DF','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
-let i = 0;
-while(i<estados.length){
-    console.log (i);
-    console.log(estados[i]);
-    i++;
+let idade = 42;
+if(idade>17){
+console.log('pode dirigir');
 }
-
-for (let index = 0; index < estados.length; index++) {
-    console.log(index);
-    console.log(estados[index]);
+else{
+console.log ('ilegal dirigir');
 }
