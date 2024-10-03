cc216013d13f09e9b3d4f9ce8cfcc685bde2cdea
manuvit123 <146022177+manuvit123@users.noreply.github.com>
2024-10-03 11:25:44 -0300

    B

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-let numero=parseInt(prompt('numero'));
-document.write(numero)
-function soma (numero1,numero2){
-    return numero1+numero2;
-}
-console.log(soma(1,2));
+// let numero=parseInt(prompt('numero'));
+// document.write(numero)
+// function soma (numero1,numero2){
+//     return numero1+numero2;
+// }
+// console.log(soma(1,2));
 
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(numero));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(numero));
 
-let numero1 = parseInt(prompt('Qual numero?'))
-let numero2 = parseInt(prompt('Qual numero?'))
-let numero3 = parseInt(prompt('Qual numero?'))
+// let numero1 = parseInt(prompt('Qual numero?'))
+// let numero2 = parseInt(prompt('Qual numero?'))
+// let numero3 = parseInt(prompt('Qual numero?'))
 
-function media (x,y,z){
-    return (x+y+z)/3;
-}
-document.write(media(numero1,numero2,numero3))
+// function media (x,y,z){
+//     return (x+y+z)/3;
+// }
+// document.write(media(numero1,numero2,numero3))
+
+let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO']
